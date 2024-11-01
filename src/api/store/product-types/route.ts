e0fbbd1a7def5a3014a5c241719e8648b3d3ed09
Farid Salmani <faridsalmaniy@gmail.com>
2024-11-01 13:13:37 +0330

    initial + product-types and product-tags

---
+++ src/api/store/product-types/route.ts
@@ -0,0 +1,15 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
+
+export async function GET(
+  req: MedusaRequest,
+  res: MedusaResponse
+): Promise<void> {
+  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+  const data = await query.graph({
+    entity: "product_types",
+    fields: ["id", "value"],
+  });
+
+  res.json(data.data);
+}
